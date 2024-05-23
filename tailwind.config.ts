510e361c68979eadcd8583e46619a46bad3ab80a
Katie Noland <essentialbreads@Katies-MacBook-Pro.local>
2024-05-22 21:11:13 -0600

    fix a typo in the tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ,
     "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [require("@tailwindcss/forms")],
