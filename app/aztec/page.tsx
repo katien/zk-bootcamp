673394ef95e010575ce5d6a876946f37171267ee
Katie Noland <essentialbreads@Katies-MacBook-Pro.local>
2024-05-29 17:35:23 -0600

    format proof in hex

--- app/aztec/page.tsx
+++ app/aztec/page.tsx
@@ -3,11 +3,10 @@ import { useState, useEffect } from "react";
 import circuit from "../../noir_circuit/target/noir_circuit.json";
 import { BarretenbergBackend } from "@noir-lang/backend_barretenberg";
 import { Noir } from "@noir-lang/noir_js";
-import { CompiledCircuit, ProofData } from "@noir-lang/types";
 
 export default function Page() {
   const [input, setInput] = useState("");
-  const [proof, setProof] = useState<ProofData>();
+  const [proof, setProof] = useState<>("");
 
   useEffect(() => {
     (async function setup() {
@@ -39,9 +38,15 @@ export default function Page() {
       const noir = new Noir(circuit, backend);
 
       let finalProof = await noir.generateFinalProof({ x: input });
-      setProof(finalProof);
+      let hexProof =
+        "0x" +
+        Array.from(finalProof.proof, (byte) =>
+          byte.toString(16).padStart(2, "0"),
+        ).join("");
+
+      setProof(hexProof);
     } catch (err) {
-      console.log(err);
+      alert(err);
     }
   };
 
@@ -52,8 +57,18 @@ export default function Page() {
       </h1>
       <div className="space-y-12">
         <div className="border-b border-white/10 pb-12">
+          <a
+            className="text-indigo-500"
+            target="_blank"
+            href={
+              "https://sepolia.etherscan.io/address/0xbd894dd2a88d0d2f7fa404b9cfd225b7ecb2749e#readContract"
+            }
+          >
+            Click here to verify your proof on a smart contract
+          </a>
           <p className="mt-1 text-sm leading-6 text-gray-400">
-            Which number multiplied by itself is equal to 25?
+            Call the verify function with the hex proof as your _proof and an
+            empty array for _publicInputs
           </p>
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
@@ -63,6 +78,9 @@ export default function Page() {
               >
                 Input
               </label>
+              <p className=" text-sm  text-gray-400">
+                Which number multiplied by itself is equal to 25?
+              </p>
               <div className="mt-2">
                 <input
                   type="text"
@@ -71,7 +89,7 @@ export default function Page() {
                   value={input}
                   onChange={(e) => setInput(e.target.value)}
                   className="block w-full rounded-md bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                  placeholder="Enter value for d"
+                  placeholder="5"
                 />
               </div>
             </div>
@@ -98,38 +116,7 @@ export default function Page() {
         <h2 className="text-base font-semibold leading-7 text-white">
           Outputs
         </h2>
-        <div className="mt-2">
-          <label
-            htmlFor="witness"
-            className="block text-sm font-medium leading-6 text-white"
-          >
-            Witness
-          </label>
-          <textarea
-            id="witness"
-            name="witness"
-            rows={3}
-            value={"witness?.toString()"}
-            readOnly
-            className="block w-full rounded-md bg-white/5 py-1.5 text-white sm:text-sm"
-          />
-        </div>
-        <div className="mt-2">
-          <label
-            htmlFor="output"
-            className="block text-sm font-medium leading-6 text-white"
-          >
-            Output
-          </label>
-          <textarea
-            id="output"
-            name="output"
-            rows={3}
-            value={"output"}
-            readOnly
-            className="block w-full rounded-md bg-white/5 py-1.5 text-white sm:text-sm"
-          />
-        </div>
+
         <div className="mt-2">
           <label
             htmlFor="proof"
@@ -141,7 +128,7 @@ export default function Page() {
             id="proof"
             name="proof"
             rows={3}
-            value={JSON.stringify(proof)}
+            value={proof}
             readOnly
             className="block w-full rounded-md bg-white/5 py-1.5 text-white sm:text-sm"
           />
@@ -151,29 +138,13 @@ export default function Page() {
             htmlFor="verifier"
             className="block text-sm font-medium leading-6 text-white"
           >
-            Verifier
+            Verifier Address
           </label>
           <textarea
             id="verifier"
             name="verifier"
             rows={3}
-            value={"verifier"}
-            readOnly
-            className="block w-full rounded-md bg-white/5 py-1.5 text-white sm:text-sm"
-          />
-        </div>
-        <div className="mt-2">
-          <label
-            htmlFor="isVerified"
-            className="block text-sm font-medium leading-6 text-white"
-          >
-            Verification Status
-          </label>
-          <textarea
-            id="isVerified"
-            name="isVerified"
-            rows={1}
-            value={"isVerified"}
+            value={"0xbd894DD2a88d0D2F7fA404b9cFd225b7ecb2749E"}
             readOnly
             className="block w-full rounded-md bg-white/5 py-1.5 text-white sm:text-sm"
           />
